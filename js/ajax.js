0ef72ded25db8408bf20e08c0c1ff52653b2e950
Sumanth <DevarakondaSumanth@JohnDeere.com>
2018-08-24 00:29:35 +0530

    error conditions and hide button

--- js/ajax.js
+++ js/ajax.js
@@ -1,14 +1,26 @@
 var ajaxButton = document.querySelector("#ajaxButton");
 var animalsDiv = document.querySelector("#animal-info");
+var pageCounter = 1;
 
 ajaxButton.addEventListener("click", function() {
     var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
 ourRequest.onload = function() {
-    var data = JSON.parse(ourRequest.responseText);
-    renderHTML(data);
+    if (ourRequest.status >= 200 && ourRequest.status < 400) {//data is received
+        var data = JSON.parse(ourRequest.responseText);
+        renderHTML(data);
+    } else {
+        console.log("We connected to the server, but it returned an error.");
+    }
+};
+ourRequest.onerror = function() {
+    console.log("Connection error");
 };
 ourRequest.send();
+pageCounter++;
+if (pageCounter > 3) {
+  ajaxButton.classList.add("hide-me");
+}
 });
 
 function renderHTML(data) {
